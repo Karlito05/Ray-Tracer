c2d2aa9cb96b1172ace112bc433d124d6b4113c2
Karel Lukes <karelos101@gmail.com>
2026-07-09 14:56:27 +0200

    added vec3 class and its aliases

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
-
+#include "color.h"
+#include "vec3.h"
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -6,22 +7,16 @@
 constexpr int IMAGE_WIDTH{256};
 constexpr int IMAGE_HEIGHT{256};
 const std::string FILENAME{"Render.ppm"};
-
 int main() {
   std::ofstream file(FILENAME, std::ios::out);
   file << "P3\n" << IMAGE_WIDTH << ' ' << IMAGE_HEIGHT << "\n255\n";
 
   for (int j = 0; j < IMAGE_HEIGHT; j++) {
+    std::clog << "\rScanlines Remaining: " << IMAGE_HEIGHT - j << std::flush;
     for (int i = 0; i < IMAGE_WIDTH; i++) {
-      auto r = double(i) / (IMAGE_WIDTH - 1);
-      auto g = double(j) / (IMAGE_HEIGHT - 1);
-      auto b = 0.0;
-
-      int ir = int(255.999 * r);
-      int ig = int(255.999 * g);
-      int ib = int(255.999 * b);
-
-      file << ir << ' ' << ig << ' ' << ib << '\n';
+      auto pixelColor = color(static_cast<double>(i) / (IMAGE_WIDTH - 1),
+                              static_cast<double>(j) / (IMAGE_HEIGHT - 1), 0);
+      write_color(file, pixelColor);
     }
   }
 }
---
+++ src/vec3.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <array>
+#include <cmath>
+#include <iostream>
+
+class vec3 {
+public:
+  std::array<double, 3> e;
+
+  double x() const { return e[0]; }
+  double y() const { return e[1]; }
+  double z() const { return e[2]; }
+
+  double r() const { return e[0]; }
+  double g() const { return e[1]; }
+  double b() const { return e[2]; }
+
+  vec3() : e{0, 0, 0} {}
+  vec3(double e1, double e2, double e3) : e{e1, e2, e3} {}
+
+  vec3 operator-() const;
+  vec3 &operator+=(const vec3 &v);
+  vec3 &operator*=(double t);
+  vec3 &operator/=(double t);
+  double operator[](std::size_t i) const;
+  double &operator[](std::size_t i);
+
+  double length() const;
+  double length_squared() const;
+};
+
+std::ostream &operator<<(std::ostream &out, const vec3 &v);
+
+vec3 operator+(const vec3 &u, const vec3 &v);
+
+vec3 operator-(const vec3 &u, const vec3 &v);
+
+vec3 operator*(const vec3 &u, const vec3 &v);
+
+vec3 operator*(double t, const vec3 &v);
+
+vec3 operator*(const vec3 &v, double t);
+
+vec3 operator/(const vec3 &v, double t);
+
+vec3 unit_vector(const vec3 &v);
+
+vec3 cross(const vec3 &u, const vec3 &v);
+
+double dot(const vec3 &u, const vec3 &v);
