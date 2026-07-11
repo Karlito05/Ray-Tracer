77e9981ea80d643ec74d000f79f020144ba66ebd
Karel Lukes <karelos101@gmail.com>
2026-07-11 11:44:25 +0200

    added fuzziness value to the metal

--- src/materials/material.cpp
+++ src/materials/material.cpp
@@ -14,7 +14,8 @@ bool lambertian::scatter(const ray &r_in, const hit_record &rec,
 bool metal::scatter(const ray &r_in, const hit_record &rec, color &attenuation,
                     ray &scattered) const {
   vec3 reflected = reflect(r_in.direction(), rec.normal);
+  reflected = unit_vector(reflected) + (fuzz * random_unit_vector());
   scattered = ray(rec.p, reflected);
   attenuation = albedo;
-  return true;
+  return (dot(scattered.direction(), rec.normal) > 0);
 }
